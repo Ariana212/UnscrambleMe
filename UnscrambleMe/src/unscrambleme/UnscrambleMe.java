b818908edd209be6222c67f48bd1e53dabb9486b
Ariana212 <95535033+Ariana212@users.noreply.github.com>
2021-12-12 23:28:42 -0500

    my commit

--- UnscrambleMe/src/unscrambleme/UnscrambleMe.java
+++ UnscrambleMe/src/unscrambleme/UnscrambleMe.java
@@ -56,6 +56,9 @@ public class UnscrambleMe {
                     else if(count == 1){
                         System.out.printf("%s%s\n\n", "Sorry, you're out of tries. The answer was: ", line);
                     }
+                    else if(count == 3){
+                        System.out.println("Hint: the words starts with the letter: " + line.charAt(0)); // char at index 0
+
             }
          }
          buffer.close() ;
